c21aa40fa98f5815e2311f1cdfdb1849cc35b0b3
scsancho <sandracatchot@gmail.com>
2024-09-30 17:17:09 +0200

    CLASE 3

---
+++ UD03 Estructura d'un programa en JavaScript/Control de flux/script.js
@@ -0,0 +1,60 @@
+//let numero = Number(prompt("Elige tu número:"));
+//console.log("Tu número es la raíz cuadrada de " + numero * numero);
+
+// Condicional
+/*let numero = Number(prompt("Elige tu número:"));
+
+if(!Number.isNaN(numero)) {
+    if (numero > 8) console.log("El número que has introducido es mayor que 8");
+    
+    console.log("La raíz cuadrada de " + numero + " es: " + (numero * numero)); 
+} else console.log("No has introducido un número");*/
+
+// else if
+// else
+
+//BUCLES
+//WHILE
+/*let numero = 2;
+let limite = 100;
+while(numero <= limite) {
+    console.log(numero);
+    numero = numero + 2;
+}*/
+
+// // SUMAR TODOS LOS NUMEROS PARES HASTA EL 100
+// let numero = 2;
+// let suma = 0;
+// let numeroLimite = 100;
+
+// while (numero <= numeroLimite) {
+//   if (numero % 2 === 0) {
+//     suma = suma + numero; // Acumula la suma de los números pares
+//   }
+//   numero = numero + 1; // Incrementa el número
+// }
+// console.log(suma);
+
+// //PEDIR DOS NúMEROS Y HACER QUE EL PRIMER SEA LA BASE Y EL SEGUNDO EL EXPONENTE EN UNA OPERACIÓN, MOSTRAR RESULTADO
+// let base = Number(prompt("Introduce el primer número (base):"));
+// let exponente = Number(prompt("Introduce el segundo número (exponente):"));
+
+// // Calcular la potencia
+// let resultado = 1;
+// contador = 0;
+
+// while (contador < exponente) {
+//     resultado *= base;
+//     contador++;
+// } 
+
+// console.log(base + " elevado a " + exponente + " es igual a: " + resultado);
+
+
+// //DO WHILE
+// let nombre;
+// do {
+//     nombre = prompt("¿Quién eres?");
+// } while (!nombre);
+
+// console.log("Nombre: " + nombre);
---
+++ UD03 Estructura d'un programa en JavaScript/Expressions i direccions/script.js
@@ -0,0 +1,28 @@
+/*let numero = 2;
+let calculo = 3 * 8;
+numero = 3;
+let compuesta = 5 + numero * calculo;
+console.log(compuesta);*/
+
+//No se pueden usar números al inicio del nombre de una variable
+//Se usan los _ para separar nombres largos de constantes
+
+/*{
+    let pepe = "pepe";
+    console.log(pepe);
+}*/
+
+//Las variables dentro de bloque solo sirven dentro del bloque, las del exterior si sirven para dentro de los bloques
+
+//console.log();
+//console.log("Hola", "adios", "requete");
+
+//prompt
+//let usuario = prompt("Entre su nombre de usuario:");
+//console.log(usuario);
+
+//let usuario = prompt("Entre su nombre de usuario:");
+//console.log(usuario || "No has escrito nada");
+
+//Valors de retorn
+//console.log(Math.max(20,200) + " és el valor màxim entre 20 i 200");
